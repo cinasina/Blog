5c42c72edf42cad9a9a2dc729c897bb0037f606b
Sina <cina_sina@yahoo.com>
2020-08-26 15:36:41 +0430

    Add Apps

--- Blog/settings.py
+++ Blog/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Home.apps.HomeConfig',
+    'Accounts.apps.AccountsConfig',
+    'Movies.apps.MoviesConfig',
 ]
 
 MIDDLEWARE = [
