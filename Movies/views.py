edfb9b7d7bcdc1a1f0d7a529c8fa1a31fa22bd23
Sina <cina_sina@yahoo.com>
2020-08-26 16:40:57 +0430

    Update Movies Views

--- Movies/views.py
+++ Movies/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from Movies.models import Movies
 
-# Create your views here.
+
+def movies_list(request):
+    movies = Movies.objects.all()
+    context = {'movies': movies}
+    return render(request, 'movies/movies_list.html', context=context)
+
+
+def movie_details(request, pk, slug):
+    details = get_list_or_404(Movies, pk=pk, slug=slug)
+    context = {'details': details}
+    return render(request, 'movies/movie_details.html', context=context)
