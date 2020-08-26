c9d0bf2d7d7d3ba0f749f012c6fc65e92883ac3c
Sina <cina_sina@yahoo.com>
2020-08-26 16:21:54 +0430

    Update Home View

--- Home/views.py
+++ Home/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'home/home.html')
+
+
+def john_mc(request):
+    return render(request, 'home/johnmc.html')
+
+
+def about(request):
+    return render(request, 'home/about.html')
