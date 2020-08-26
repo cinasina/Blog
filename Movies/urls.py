18377a6016227eb5e75f8d593e92eb945ab1af5e
Sina <cina_sina@yahoo.com>
2020-08-26 16:41:13 +0430

    Update Movies Urls

---
+++ Movies/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from Movies.views import movies_list, movie_details
+
+urlpatterns = [
+    path('list/', movies_list, name='movies_list'),
+    path('details/<int:pk>/<slug:slug>/', movie_details, name='movie_details'),
+]
